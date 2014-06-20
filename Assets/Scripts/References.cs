b9b951c8113c185ff40a50f86401656e71a49f42
choiie <toivo.s@live.com>
2014-06-20 17:51:24 +0200

    commented all scripts

---
+++ Assets/Scripts/References.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class References : MonoBehaviour {
+
+	public GameObject playerRef;
+	public GameObject enemyRef;
+	public string[] charArray; //names of objects tied to each available character attached to TemplateHero and TemplateEnemy
+
+	// Use this for initialization
+	void Start () {
+		charArray = new string[] {"PearGuy","RedPear"};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
